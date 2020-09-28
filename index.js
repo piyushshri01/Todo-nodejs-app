9b76e073d4587fed566f7d71f7220bc03a0c19a3
piyushshri01 <piyushshrimal94@gmail.com>
2020-09-28 16:43:02 +0530

    "install ejs template lib. and download some files and setup local server by express"

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello")
+})
+
+app.listen(3000, (res) => {
+    console.log("Listening to port 3000");
+})
